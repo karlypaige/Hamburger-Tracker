54c4fedc66f21633b00280f2da45aab4ffed270e
Karly Maruyama <karlypaige@yahoo.com>
2021-01-10 19:03:25 -0800

    commit to update any files that need it

--- db/seeds.sql
+++ db/seeds.sql
@@ -2,5 +2,5 @@ USE hamburgers_db;
 
 -- Insert a set of records.
 INSERT INTO hamburgers (burger, devoured) VALUES ('Plain burger.', 0);
-INSERT INTO hamburgers (burger, devoured) VALUES ('In-N-Out Double Double.', 0);
+INSERT INTO hamburgers (burger, devoured) VALUES ('In-N-Out Double Double.', 1);
 INSERT INTO hamburgers (burger, devoured) VALUES ('Burger Lounge Paleo Burger.', 0);
